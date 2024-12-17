56f789c72e38878fc7048baa029197d5c707fea3
Kaycee124 <58170271+Kaycee124@users.noreply.github.com>
2024-12-17 08:03:28 +0100

    Updated database.module.ts

--- src/core/database/database.module.ts
+++ src/core/database/database.module.ts
@@ -18,6 +18,7 @@ import * as entities from '../database/entities';
         password: configService.get('DATABASE_PASS'),
         database: configService.get('DATABASE_NAME'),
         entities: Object.values(entities),
+        ssl: false,
         synchronize: false,
         logging: configService.get('NODE_ENV') !== 'production',
         pool: {
